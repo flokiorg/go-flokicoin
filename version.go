fa96bbd73244bafae55d11e9b5d5abc2f94c48a3
mechion <4387895+mechion@users.noreply.github.com>
2025-06-16 15:48:34 +0000

    bump version 0.25.7-dev

--- version.go
+++ version.go
@@ -19,11 +19,11 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 0
 	appMinor uint = 25
-	appPatch uint = 6
+	appPatch uint = 7
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	appPreRelease = "beta"
+	appPreRelease = "dev"
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
